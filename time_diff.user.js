034ff33ab45e4bbf9ffcf84ece28aa4314b299ae
杨洋 <158954945@qq.com>
2019-09-27 11:00:24 +0800

    0.4.5

--- time_diff.user.js
+++ time_diff.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         time_diff
 // @namespace    http://boc.ink/
-// @version      0.4.4
+// @version      0.4.5
 // @description  try to take over the world!
 // @author       YY
 // @match        *://vip.win007.com/changeDetail/handicap.aspx*
@@ -416,6 +416,8 @@ Odds.prototype.getOdd = function (home) {
                     yAxis: [{
                         type: 'value',
                         scale: true,
+                        min: 0.6,
+                        max: 1.2,
                     }],
                     series: series,
                     axisPointer: {
